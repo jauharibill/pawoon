b241b28000312ca42e0b83de9201d9ef2d214685
seviansyah <seviansyahf@gmail.com>
2018-10-10 21:43:47 +0700

    Update menu/create.php

--- menu/create.php
+++ menu/create.php
@@ -5,9 +5,44 @@ if(!isset($_SESSION['login'])) {
 	exit;
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INDEX MENU</title>
+    <script type="text/javascript" src="../jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="icon" href="../img/kopi.png">
+    <style>
+         body{ 
+                background-image:url(../img/);  
+                background-repeat:no-repeat;
+                background-position:top; 
+                background-size:1400px; 
+                border: 2px; 
+                        }
 
-<form action="store.php" method="POST">
-<input type="text" name="nama" placeholder="Nama Pesanan">
-<input type="number" name="harga" placeholder="Harga">
-<button>Simpan</button>
-</form> 
+         #ex3{
+                  width: auto; box-shadow: 0px 0px 0px 0px; 
+                  border-radius: 3px; color:#fff ; 
+                  background: rgba(23,20,20,0.52); 
+                  padding: 5px 5px 5px 10px; 
+                  font-family: comic sans ms; 
+                  text-align: center; 
+                  width: auto; 
+                    }
+    </style>
+	<title>Create</title>
+</head>
+<body>
+		<form action="store.php" method="POST">
+		<input class="" type="text" name="nama" placeholder="Nama Pesanan">
+		<input class="" type="number" name="harga" placeholder="Harga">
+		<button class="btn btn-primary" >Simpan</button>
+		</form>
+</body>
+</html>
+ 
