a6a3947635ace34a9a2f6cc5294434f8521e047f
Akbar Yusuf <40377648+akbr-ysf@users.noreply.github.com>
2018-10-31 23:00:59 +0700

    remove button close
    remove button close from modal in the form.php

--- form.php
+++ form.php
@@ -117,9 +117,6 @@ if(!isset($_SESSION['login'])) {
 		        		<div class="modal-content">
 		        			<div class="modal-header">
 		              			<h5 class="modal-title" id="simpanTitle">Tersimpan</h5>
-		              			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-		              				<span aria-hidden="true">&times;</span>
-		              			</button>
 		            		</div>
 		            		<div class="modal-body">
 		              			Data Sukses Tersimpan
